28294c9edd54754be1a2df65f6b3f7a95a3e494d
Jose Daniel Organista <jodorganistaca@unal.edu.co>
2019-09-12 17:25:45 -0500

    Add files via upload

---
+++ app/Http/Controllers/AppController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+class AppController extends Controller
+{
+    public function init(){
+        $user = Auth::user();
+
+        return response()->json(['user' => $user], 200);
+    }
+
+    public function login(Request $request){
+        if(Auth::attemp(['username' => $request->username, 'password' => $password], true)){
+            return response()->json(Auth::user(), 200);
+        }else{
+            return response()->json(['error' => 'Could not log in.'], 401);
+        }
+    }
+
+    public function register(Request $request){
+        $user = User::where('username', $request->username)->first();
+
+        if(isset($user->id)){
+            return response()->json(['error' => 'Username already exists.'], 401);
+        }
+        
+        $user = new User();
+
+        $user->username = $request->username;
+        $user->password =  bcrypt($request->password);
+        $user->save();
+
+        Auth::login($user);
+
+        return response()->json($user, 200);
+    }
+
+    public function logout(){
+        Auth::logout();
+    }
+}
